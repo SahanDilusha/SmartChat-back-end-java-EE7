57409a69dbe9f35e5f4e0e0014535a3573447702
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-28 13:21:47 +0530

    start sign up

--- src/java/controller/SignUp.java
+++ src/java/controller/SignUp.java
@@ -2,11 +2,13 @@ package controller;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import entity.User;
+import entity.UserStatus;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +16,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import model.HibernateUtil;
 import model.Validations;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @MultipartConfig
 @WebServlet(name = "SignUp", urlPatterns = {"/SignUp"})
@@ -24,38 +30,68 @@ public class SignUp extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         try {
-
-            JsonObject responseObject = new JsonObject();
-            String mobile = req.getParameter("mobile");
-            String firstName = req.getParameter("password");
-            String lastName = req.getParameter("first_name");
-            String password = req.getParameter("last_name");
-
-            Part image = req.getPart("avatarImage");
-            
-            if (mobile.trim().isEmpty()) {
-                
-            }else if (!Validations.isMobile(mobile)) {
-                
-            }  else if (firstName.trim().isEmpty()) {
-                
-            }else if (lastName.trim().isEmpty()) {
-                
-            }else if (!Validations.isPasswordValid(password)) {
-                
-            }
-            
-
-            String serverPath = req.getServletContext().getRealPath("");
-
-            String avatarImagePath = serverPath + File.separator + "avatar_image" + File.separator + mobile + ".png";
-            File file = new File(avatarImagePath);
-
-            Files.copy(image.getInputStream(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
             Gson gson = new Gson();
+            JsonObject responseObject = new JsonObject();
+            responseObject.addProperty("success", false);
 
-            responseObject.addProperty("message", "Client:Hello");
+            String mobile = req.getParameter("mobile");
+            String password = req.getParameter("password");
+            String firstName = req.getParameter("first_name");
+            String lastName = req.getParameter("last_name");
+
+            System.out.println(password);
+
+            Part avatarImage = req.getPart("avatarImage");
+
+            if (mobile.trim().isEmpty()) {
+                responseObject.addProperty("message", "Please fill  your Mobile Number");
+            } else if (!Validations.isMobile(mobile)) {
+                responseObject.addProperty("message", "Invalid mobile number");
+            } else if (firstName.trim().isEmpty()) {
+                responseObject.addProperty("message", "Please fill  your first name");
+            } else if (lastName.trim().isEmpty()) {
+                responseObject.addProperty("message", "Please fill  your last name");
+            } else if (!Validations.isPasswordValid(password)) {
+                responseObject.addProperty("message", "Invalid password");
+            } else {
+
+                Session session = HibernateUtil.getSessionFactory().openSession();
+
+                Criteria criteria = session.createCriteria(User.class).add(Restrictions.eq("mobile", mobile));
+
+                if (!criteria.list().isEmpty()) {
+                    responseObject.addProperty("message", "Mobile number already Used.");
+                } else {
+
+                    User user = new User();
+
+                    user.setFirst_name(firstName);
+                    user.setLast_name(lastName);
+                    user.setMobile(mobile);
+                    user.setPassword(password);
+                    user.setRegistered_date_time(new Date());
+
+                    UserStatus userStatus = (UserStatus) session.get(UserStatus.class, 2);
+
+                    user.setUserStatus(userStatus);
+
+                    session.save(user);
+                    session.beginTransaction().commit();
+
+                    if (avatarImage != null) {
+                        String serverPath = req.getServletContext().getRealPath("");
+
+                        String avatarImagePath = serverPath + File.separator + "avatar_image" + File.separator + mobile + ".png";
+                        File file = new File(avatarImagePath);
+
+                        Files.copy(avatarImage.getInputStream(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+                    }
+
+                    responseObject.addProperty("success", true);
+                    responseObject.addProperty("message", "Registration Complete");
+                }
+            }
 
             resp.setContentType("application/json");
             resp.getWriter().write(gson.toJson(responseObject));
